adfb14f4f6b75c943bea1c67d4a36913465c1081
Lionel <lionel.collidor2016@campus-eni.fr>
2017-02-27 21:22:08 +0100

    commit implÃementation main #1

--- apps/operateurs-console/src/fr/operateurs/console/OperateursApp.java
+++ apps/operateurs-console/src/fr/operateurs/console/OperateursApp.java
@@ -1,10 +1,26 @@
 package fr.operateurs.console;
 
+import java.util.Scanner;
+
 public class OperateursApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("**** Application Opérateurs ****");
+		
+		System.out.println("Veuillez saisir le premier nombre");
+		int nb1 = sc.nextInt();
+		
+		System.out.println("Veuillez saisir le second nombre");
+		int nb2 = sc.nextInt();
+		
+		System.out.println(nb1 + " + " + nb2 + " = " + (nb1 + nb2));
+		System.out.println(nb1 + " - " + nb2 + " = " + (nb1 - nb2));
+		System.out.println(nb1 + " * " + nb2 + " = " + (nb1 * nb2));
+		System.out.println(nb1 + " / " + nb2 + " = " + (nb1 / nb2));
+		System.out.println(nb1 + " % " + nb2 + " = " + (nb1 % nb2));
+		
 	}
 
 }
