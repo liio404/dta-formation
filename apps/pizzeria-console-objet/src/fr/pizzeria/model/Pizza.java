73523a4990461de082928b273d945a167e9eea80
Lionel <lionel.collidor2016@campus-eni.fr>
2017-03-06 22:20:34 +0100

    impementation objet pizza et test console app #3

--- apps/pizzeria-console-objet/src/fr/pizzeria/model/Pizza.java
+++ apps/pizzeria-console-objet/src/fr/pizzeria/model/Pizza.java
@@ -1,5 +1,65 @@
 package fr.pizzeria.model;
 
 public class Pizza {
+	
+	private int id;
+	private String code;
+	private String nom;
+	private double prix;
+	private static int nbPizza;
+	
+	public Pizza() {
+		super();
+		this.nbPizza ++;
+	}
+	
+	public Pizza(int id, String code, String nom, double prix){
+		this();
+		this.id = id;
+		this.code = code;
+		this.nom = nom;
+		this.prix = prix;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public static int getNbPizza() {
+		return nbPizza;
+	}
+
+	@Override
+	public String toString() {
+		return this.code + " - " + this.nom + " (" + this.prix + ")";
+	}
+	
 }
