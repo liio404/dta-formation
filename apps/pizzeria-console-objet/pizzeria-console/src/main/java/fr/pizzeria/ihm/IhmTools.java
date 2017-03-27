a685b22487080acad557c07c33a84837574ce3fa
Lionel <lionel.collidor2016@campus-eni.fr>
2017-03-27 21:35:32 +0200

    correction merge confilt

--- apps/pizzeria-console-objet/pizzeria-console/src/main/java/fr/pizzeria/ihm/IhmTools.java
+++ apps/pizzeria-console-objet/pizzeria-console/src/main/java/fr/pizzeria/ihm/IhmTools.java
@@ -5,39 +5,12 @@ import java.util.Scanner;
 
 import fr.pizzeria.dao.IPizzaDao;
 import fr.pizzeria.dao.PizzaDaoImpl;
-<<<<<<< HEAD
-=======
 import fr.pizzeria.dao.PizzaDaoImplFichier;
->>>>>>> dvp1
 import fr.pizzeria.model.Pizza;
 
 public class IhmTools {
 	
-<<<<<<< HEAD
-	private final static Scanner SC = new Scanner(System.in);
-	//private final static IPizzaDao<Pizza, String> DAO = new PizzaDaoImplFichier();
-	private IPizzaDao<Pizza, String> daoImpl;
-	
-	public IhmTools(){
-		ResourceBundle bundle = ResourceBundle.getBundle("application");
-		String impl = bundle.getString("dao.impl");
-		
-		try {
-			this.daoImpl = (IPizzaDao<Pizza, String>) Class.forName(impl).newInstance();
-			
-			
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-			// implémentation mémoire par défaut au cas ou
-			this.daoImpl = new PizzaDaoImpl();
-			//throw new StockageException("Impossible d'avoir les données de l'application",e);
-		}
-	}
 
-	public Scanner getSc() {
-		return SC;
-	}
-	
-=======
 	private final static Scanner SC = new Scanner(System.in);;
 	private IPizzaDao<Pizza, String> dao;
 	//private final static IPizzaDao<Pizza, String> DAO = new PizzaDaoImplFichier();
@@ -47,7 +20,7 @@ public class IhmTools {
 		String daoImpl = bundle.getString("dao.impl");
 		
 		try {
-			dao = (IPizzaDao<Pizza, String>) Class.forName(daoImpl).newInstance();
+			this.dao = (IPizzaDao<Pizza, String>) Class.forName(daoImpl).newInstance();
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -62,13 +35,8 @@ public class IhmTools {
 		return this.SC;
 	}
 
->>>>>>> dvp1
 	public void close() {
 		this.SC.close();
 	}
 
-	public IPizzaDao<Pizza, String> getDao() {
-		return this.daoImpl;
-	}
-	
 }
--- apps/pizzeria-console-objet/pizzeria-dao/src/main/java/fr/pizzeria/dao/IPizzaDao.java
+++ apps/pizzeria-console-objet/pizzeria-dao/src/main/java/fr/pizzeria/dao/IPizzaDao.java
@@ -2,27 +2,14 @@ package fr.pizzeria.dao;
 
 import java.util.List;
 
-<<<<<<< HEAD
-import fr.pizzeria.exception.DeletePizzaException;
-import fr.pizzeria.exception.SavePizzaException;
-import fr.pizzeria.exception.StockageException;
-import fr.pizzeria.exception.UpdatPizzaException;
-
-public interface IPizzaDao<T,E> {
-=======
 import fr.pizzeria.exception.StockageException;
 
 public interface IPizzaDao<T, E> {
->>>>>>> dvp1
 	
 	List<T> findAll();
 	boolean save(T obj) throws StockageException;
 	boolean update(E code, T obj) throws StockageException;
 	boolean delete(E code) throws StockageException;
-<<<<<<< HEAD
 	T find(E code);
-=======
-	T find(String code);
->>>>>>> dvp1
 
 }
